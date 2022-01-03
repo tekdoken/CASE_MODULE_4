f0c340476fec09ba9981af803e1fa0f084e27d14
tekdoken <tekdokenzx@gmail.com>
2022-01-03 15:50:57 +0700

    student, parent controller

--- src/main/java/com/example/case_module_4/controller/StudentController.java
+++ src/main/java/com/example/case_module_4/controller/StudentController.java
@@ -6,28 +6,43 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+import java.util.Optional;
+@CrossOrigin("*")
+@RestController
 @RequestMapping("/api/students")
 public class StudentController {
     @Autowired
     private IStudentService studentService;
-//    Iterable<House> houses = houseService.findAll();
+
+    //    Iterable<House> houses = houseService.findAll();
 //        if (houses == null) {
 //            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
 //        }
 //        return new ResponseEntity<>(houses, HttpStatus.OK);
     @GetMapping
-    public ResponseEntity<Iterable<Student>> findAll(){
+    public ResponseEntity<Iterable<Student>> findAll() {
         Iterable<Student> students = studentService.findAll();
-        if(students == null){
+        if (students == null) {
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
         }
-        return new ResponseEntity<>(students,HttpStatus.OK);
+        return new ResponseEntity<>(students, HttpStatus.OK);
     }
 
+    @PostMapping("")
+    public ResponseEntity<Student> add(@RequestBody Student student) {
+        return new ResponseEntity<>(studentService.save(student), HttpStatus.OK);
+    }
 
+    @PutMapping("/{id}")
+    public ResponseEntity<Student> update(@PathVariable Long id, @RequestBody Student student) {
+        student.setId(id);
+        return new ResponseEntity<>(studentService.save(student), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<Student>> findOne(@PathVariable Long id) {
+        return new ResponseEntity<>(studentService.findById(id), HttpStatus.OK);
+    }
 }
