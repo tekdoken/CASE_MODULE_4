43c7875aebf0b585261eab44fa7ce28fdccbef91
hoangfvi09 <huonglt129@gmail.com>
2022-01-04 21:52:52 +0700

    register, encode password ok, jwt ok

--- src/main/java/com/example/case_module_4/service/impl/UserService.java
+++ src/main/java/com/example/case_module_4/service/impl/UserService.java
@@ -2,13 +2,13 @@ package com.example.case_module_4.service.impl;
 
 import com.example.case_module_4.model.Parent;
 import com.example.case_module_4.model.User;
-//import com.example.case_module_4.model.UserPrinciple;
+import com.example.case_module_4.model.UserPrinciple;
 import com.example.case_module_4.repository.UserRepository;
 import com.example.case_module_4.service.IRoleService;
 import com.example.case_module_4.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.security.core.userdetails.UserDetails;
-//import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -39,14 +39,14 @@ public class UserService implements IUserService {
         userRepository.deleteById(id);
     }
 
-//    @Override
-//    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-//        Optional<User> userOptional = userRepository.findByUsername(username);
-//        if (!userOptional.isPresent()) {
-//            throw new UsernameNotFoundException(username);
-//        }
-//        return UserPrinciple.build(userOptional.get());
-//    }
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> userOptional = userRepository.findByUsername(username);
+        if (!userOptional.isPresent()) {
+            throw new UsernameNotFoundException(username);
+        }
+        return UserPrinciple.build(userOptional.get());
+    }
 
     @Override
     public Optional<User> findByUsername(String username) {
